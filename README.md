da95c1c4ce30b0e883acc99c61c94b78372067da
Jiajun Zhu <j-zhu@creationline.com>
2019-06-02 20:06:07 +0900

    extract nodes and relationships from WordNet xml file.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# wordnet-graph-neo4j
+This is an implementation for importing WordNet data into neo4j.
+
+# reference
+
+http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.672.2336&rep=rep1&type=pdf
--- wordnet_manager.py
+++ wordnet_manager.py
@@ -1,8 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from xml.etree import ElementTree
 
 class WordNetManger:
     def __init__(self, file_name):
         self.file_name = file_name
+
+        # word nodes (property: lemma,pos)
+        self.words = {}
+        # synsets nodes (property: id,def)
+        self.synsets = {}
+        # relationships between words and synsets (property: pos)
+        self.rel_sence = {}
+        # relationships between synsets (property: relType)
+        self.rel_sementic = {}
 
     def _parse(self):
         # XML ファイルから ElementTree オブジェクトを生成
@@ -13,6 +25,9 @@ class WordNetManger:
         return root
 
     def create_csv_for_neo4j(self):
+        self._expend_xml_on_memory()
+
+    def _expend_xml_on_memory(self):
         # neo4jインポート用のcsvファイルを作成
         root = self._parse()
         for child in root:
@@ -20,18 +35,42 @@ class WordNetManger:
             if child.tag == "Lexicon":
                 for resource in child:
                     if resource.tag == "LexicalEntry":
+                        word_id = resource.attrib["id"]
                         for entry in resource:
                             if entry.tag == "Lemma":
-                                print(entry.tag, entry.attrib)
+                                word_lemma = entry.attrib["writtenForm"]
+                                word_pos = entry.attrib["partOfSpeech"]
+                                print("Lemma:", word_lemma, word_pos)
+                            elif entry.tag == "Sense":
+                                synset_id = entry.attrib["synset"]
+                                print("  Sense synset id", synset_id)
                             else:
-                                print(entry.tag, entry.attrib)
-                        break
+                                pass
                     elif resource.tag == "Synset":
+                        synset_id = resource.attrib["id"]
+                        synset_pos = synset_id.split("-")[-1]
+                        print("Synset id", synset_id)
                         for entry in resource:
-                            print(entry.tag, entry.attrib)
-                        break
+                            if entry.tag == "Definition":
+                                definition = entry.attrib["gloss"]
+                                print("  Definition:", definition)
+                            elif entry.tag == "SynsetRelations":
+                                for relation in entry:
+                                    rel_target_id = relation.attrib["targets"]
+                                    rel_target_type = relation.attrib["relType"]
+                                    print("    related synset:", rel_target_id, rel_target_type)
+                            else:
+                                pass
                     else:
                         pass
+            else:
+                pass
+
+    def _create_node(self):
+        pass
+
+    def _create_relationship(self):
+        pass
 
 if __name__ == "__main__":
     wordNetManger = WordNetManger("jpn_wn_lmf.xml")
